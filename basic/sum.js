02795c2a3653da9d273568e1f371972138dcade6
yeu-shiuan <rain020639@gmail.com>
2021-05-22 16:34:33 +0800

    add sum.js

---
+++ basic/sum.js
@@ -0,0 +1,12 @@
+function sum(n){
+    let sum = 0
+    for(let i=1; i<=n; i++){
+        sum += i
+    }
+    return sum
+}
+
+console.log(sum(1))
+console.log(sum(2))
+console.log(sum(10))
+console.log(sum(100000))
