41bb457437615e4c21927b0d3df62824c3b1c086
yeu-shiuan <rain020639@gmail.com>
2021-05-29 13:04:15 +0800

    app.js 新增

--- crawler/app.js
+++ crawler/app.js
@@ -1,5 +1,21 @@
-//https://www.twse.com.tw/exchangeReport/STOCK_DAY
+//https://www.twse.com.tw/exchangeReport/STOCK_DAY?
 //?response=json
 //&date=20210523
 //&stockNo=2610
 
+const axios = require("axios");
+
+axios
+.get(
+    "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210523&stockNo=2610"
+)
+.then(function (response){
+    console.log(response);
+    if(response.data.stat === "OK"){
+        console.log(response.data.data);
+        console.log(response.data.title);
+    }
+})
+.catch(function(error){
+    console.log(error);
+})
