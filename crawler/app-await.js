679bbfa815c70d97f0c919a327c9bb2de44edc74
yeu-shiuan <rain020639@gmail.com>
2021-05-30 15:39:44 +0800

    add app-await

---
+++ crawler/app-await.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+
+function readFilePromise() {
+return new Promise((resolve, reject) => {
+           fs.readFile("stock.txt", "utf8", (err, data) => {
+                if (err) {
+                    reject(err);
+                }else{
+                    resolve(data);
+                }
+            })
+        })
+    }
+    
+    (async function() {
+        try {
+            let stockNo = await readFilePromise();
+            let result = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+                params: {
+                    response: JSON,
+                    date: moment().format("YYYYMMDD"),
+                    stockNo: stockNo
+                }
+            });
+            console.log(result.data.data);
+            console.log(result.data.title);
+        } catch (err) {
+            console.log(err);
+        };
+    })();
+
+        
