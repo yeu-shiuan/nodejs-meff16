23c7fadd15bece978c2095132114a1ff0650b7f0
yeu-shiuan <rain020639@gmail.com>
2021-05-30 12:50:07 +0800

    add app-promise.js

---
+++ crawler/app-promise.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+
+const fs = require("fs");
+
+// fs.readFile("stock.txt", "utf8", (err, data) => {
+//   if (err) {
+//     return console.error("讀檔錯誤", err);
+//   }
+//   console.log(`讀到的 stock code: ${data}`);
+
+function readFilePromise() {
+return new Promise((resolve, reject) => {
+           fs.readFile("stock.txt", "utf8", (err, data) => {
+                if (err) {
+                    reject(err);
+                }else{
+                    resolve(data);
+                }
+            })
+        })
+    }
+    
+    readFilePromise()
+            
+            .then((data) => {
+                axios
+            .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+            params: {
+                response: "json",
+                date: "20210523",
+                stockNo: data,
+            },
+            })
+            .then(function (response) {
+            if (response.data.stat === "OK") {
+                console.log(response.data.date);
+                console.log(response.data.title);
+            }
+            });
+        })
