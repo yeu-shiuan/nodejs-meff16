da4e6532e19d8461c5b2c04f9457d10b27ad9d75
yeu-shiuan <rain020639@gmail.com>
2021-05-30 12:52:15 +0800

    axios promise

--- crawler/app.js
+++ crawler/app.js
@@ -1,21 +1,65 @@
-//https://www.twse.com.tw/exchangeReport/STOCK_DAY?
-//?response=json
-//&date=20210523
-//&stockNo=2610
 
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY
+// ?response=json
+// &date=20210523
+// &stockNo=2610
+
+// npm i axios
+// 引入 axios
 const axios = require("axios");
 
-axios
-.get(
-    "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210523&stockNo=2610"
-)
-.then(function (response){
-    console.log(response);
-    if(response.data.stat === "OK"){
-        console.log(response.data.data);
-        console.log(response.data.title);
+// TODO: 從 stock.txt 讀股票代碼進來
+// filesystem
+// npm i fs ??? -> 不用
+const fs = require("fs");
+
+// fs.readFile("stock.txt", "utf8", (err, data) => {
+//   if (err) {
+//     return console.error("讀檔錯誤", err);
+//   }
+//   console.log(`讀到的 stock code: ${data}`);
+
+//   axios
+//     .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//       params: {
+//         response: "json",
+//         date: "20210523",
+//         stockNo: data,
+//       },
+//     })
+//     .then(function (response) {
+//       if (response.data.stat === "OK") {
+//         console.log(response.data.date);
+//         console.log(response.data.title);
+//       }
+//     });
+// });
+
+const fs = require("fs");
+
+function fsPromise() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("crawler/stock.txt", "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+fsPromise()
+  .then((data) => {
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: "20210523",
+        stockNo: data,
+      },
+    });
+  })
+  .then(function (response) {
+    if (response.data.stat === "OK") {
+      console.log(response.data.date);
+      console.log(response.data.title);
     }
-})
-.catch(function(error){
-    console.log(error);
-})
+  });
