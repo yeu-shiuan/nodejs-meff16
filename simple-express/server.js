c72a7001889c2fbe8a31b9e9518550e9820366ee
yeu-shiuan <rain020639@gmail.com>
2021-06-20 10:23:37 +0800

    add login/register

--- simple-express/server.js
+++ simple-express/server.js
@@ -29,6 +29,10 @@ let stockRouter =require("./routes/stock");
 app.use("/stock",stockRouter)
 let apiRouter =require("./routes/api");
 app.use("/api",apiRouter)
+let authRouter =require("./routes/auth");
+app.use("/auth",authRouter)
+//網址可自由命名
+
 
 // 路由router  (request, response){} 去回應這個請求
 // 由上而下找，找到就停住了，不會在往下一個同樣的執行
